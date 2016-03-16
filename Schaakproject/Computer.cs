5a302cddbe146f3ec867b817355cc6f41c63c5bb
roodkapje <nielssteensma@yahoo.nl>
2016-03-16 11:05:57 +0100

    Alle Pionen werken nu de 1ste ronde in Singleplayer

--- Schaakproject/Computer.cs
+++ Schaakproject/Computer.cs
@@ -143,7 +143,7 @@ namespace Schaakproject
                 Console.WriteLine("Sokolsky Opening");
                 _tegenstanderstactiek = "Sokolsky Opening";
                 selected = _spel.selected.vakje.buurNoord.buurNoord.buurNoord.buurOost.buurOost.pbox;   // geselecteerd stuk
-                pictures = _spel.selected.vakje.buurNoord.buurOost.buurOost.pbox;    // geselecteerd vak
+                pictures = _spel.selected.vakje.buurNoord.buurOost.buurOost.pbox;                       // geselecteerd vak
                 voerZetUit();
             }
 
@@ -152,11 +152,90 @@ namespace Schaakproject
             {
                 Console.WriteLine("Hungarian Opening");
                 _tegenstanderstactiek = "Hungarian Opening";
-                selected = _spel.selected.vakje.buurNoord.buurNoord.buurNoord.buurNoord.buurWest.buurWest.buurWest.pbox;   // geselecteerd stuk
-                pictures = _spel.selected.vakje.buurNoord.buurNoord.buurWest.buurWest.buurWest.pbox;     // geselecteerd vak
+                selected = _spel.selected.vakje.buurNoord.buurNoord.buurNoord.buurNoord.buurWest.buurWest.buurWest.pbox;    // geselecteerd stuk
+                pictures = _spel.selected.vakje.buurNoord.buurNoord.buurWest.buurWest.buurWest.pbox;                        // geselecteerd vak
                 voerZetUit();
             }
 
+            // "Ware Opening"
+            else if(_positieWest == 1 && _positieZuid == 4 && _vorigschaakstuk is Pion)
+            {
+                Console.WriteLine("Ware Opening");
+                _tegenstanderstactiek = "Ware Opening";
+                selected = _spel.selected.vakje.buurNoord.buurNoord.buurNoord.buurOost.buurOost.buurOost.buurOost.pbox;    // geselecteerd stuk
+                pictures = _spel.selected.vakje.buurNoord.buurOost.buurOost.buurOost.buurOost.pbox;                        // geselecteerd vak
+                voerZetUit();
+            }
+
+            // "Clemenz Opening"
+            else if (_positieWest == 8 && _positieZuid == 3 && _vorigschaakstuk is Pion)
+            {
+                Console.WriteLine("Clemenz Opening");
+                _tegenstanderstactiek = "Clemenz Opening";
+                selected = _spel.selected.vakje.buurNoord.buurNoord.buurNoord.buurNoord.buurWest.buurWest.buurWest.buurWest.pbox;    // geselecteerd stuk
+                pictures = _spel.selected.vakje.buurNoord.buurNoord.buurWest.buurWest.buurWest.buurWest.pbox;                        // geselecteerd vak
+                voerZetUit();
+            }
+
+            // "Desprez Opening"
+            else if (_positieWest == 8 && _positieZuid == 4 && _vorigschaakstuk is Pion)
+            {
+                Console.WriteLine("Desprez Opening");
+                _tegenstanderstactiek = "Desprez Opening";
+                selected = _spel.selected.vakje.buurNoord.buurNoord.buurNoord.buurWest.buurWest.buurWest.buurWest.pbox;    // geselecteerd stuk
+                pictures = _spel.selected.vakje.buurNoord.buurWest.buurWest.buurWest.buurWest.pbox;                        // geselecteerd vak
+                voerZetUit();
+            }
+
+            // "Grob Attack"
+            else if (_positieWest == 7 && _positieZuid == 4 && _vorigschaakstuk is Pion)
+            {
+                Console.WriteLine("Grob Attack");
+                _tegenstanderstactiek = "Grob Attack";
+                selected = _spel.selected.vakje.buurNoord.buurNoord.buurNoord.buurWest.buurWest.pbox;    // geselecteerd stuk
+                pictures = _spel.selected.vakje.buurNoord.buurWest.buurWest.pbox;                        // geselecteerd vak
+                voerZetUit();
+            }
+
+            // "Barnes Opening"
+            else if (_positieWest == 6 && _positieZuid == 3 && _vorigschaakstuk is Pion)
+            {
+                Console.WriteLine("Barnes Opening");
+                _tegenstanderstactiek = "Barnes Opening";
+                selected = _spel.selected.vakje.buurNoord.buurNoord.buurNoord.buurNoord.buurWest.pbox;    // geselecteerd stuk
+                pictures = _spel.selected.vakje.buurNoord.buurNoord.buurWest.pbox;                        // geselecteerd vak
+                voerZetUit();
+            }
+
+            // "Van 't kruijs Opening"
+            else if (_positieWest == 5 && _positieZuid == 3 && _vorigschaakstuk is Pion)
+            {
+                Console.WriteLine("Van 't kruijs Opening");
+                _tegenstanderstactiek = "Van 't kruijs Opening";
+                selected = _spel.selected.vakje.buurNoord.buurNoord.buurNoord.buurNoord.pbox;    // geselecteerd stuk
+                pictures = _spel.selected.vakje.buurNoord.buurNoord.pbox;                        // geselecteerd vak
+                voerZetUit();
+            }
+
+            // "Mieses Opening"
+            else if (_positieWest == 4 && _positieZuid == 3 && _vorigschaakstuk is Pion)
+            {
+                Console.WriteLine("Mieses Opening");
+                _tegenstanderstactiek = "Mieses Opening";
+                selected = _spel.selected.vakje.buurNoord.buurNoord.buurNoord.buurNoord.buurOost.pbox;    // geselecteerd stuk
+                pictures = _spel.selected.vakje.buurNoord.buurNoord.buurOost.pbox;                        // geselecteerd vak
+                voerZetUit();
+            }
+
+            // "Saragossa Opening"
+            else if (_positieWest == 3 && _positieZuid == 3 && _vorigschaakstuk is Pion)
+            {
+                Console.WriteLine("Saragossa Opening");
+                _tegenstanderstactiek = "Saragossa Opening";
+                selected = _spel.selected.vakje.buurNoord.buurNoord.buurNoord.buurNoord.pbox;    // geselecteerd stuk
+                pictures = _spel.selected.vakje.buurNoord.buurNoord.pbox;                        // geselecteerd vak
+                voerZetUit();
+            }
         }
 
         private void voerZetUit()
