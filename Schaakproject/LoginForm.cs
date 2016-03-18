da99aa6b3728663332a63585c536a6c5c52cc444
Daan Nieuwenburg <15070735@student.hhs.nl>
2016-03-19 00:06:14 +0100

    Classe database toegevoegd. Hij vind database uit(denk ik) Alleen t uitlezen lukt nog niet echt

--- Schaakproject/LoginForm.cs
+++ Schaakproject/LoginForm.cs
@@ -17,6 +17,7 @@ namespace Schaakproject
         public bool login { get; private set; }
         public string username { get; set; }
         public string password { get; set; }
+        
 
         public LoginForm()
         {
@@ -27,31 +28,46 @@ namespace Schaakproject
         }
 
         private void btnLogin_Click(object sender, EventArgs e)
-        {        
-            if ((txtUsername.Text == username && txtPassword.Text == password) || (txtUsername.Text == "test" && txtPassword.Text == "test"))
+        {
+            Database database = new Database();
+            database.connect();
+            for (int i = 0; i<8; i++)
             {
-                username = txtUsername.Text.ToString();
-                Console.WriteLine("Loginform username: " + username);
-                Console.WriteLine("Loginform txtusername: " + txtUsername.Text);
-                login = true;
-                label3.Text = login.ToString();
-                Close();
+                if (database.Username[i] == txtUsername.Text && database.Password[i] == txtPassword.Text)
+                {
+                    username = txtUsername.Text.ToString();
+                    Console.WriteLine("Loginform username: " + username);
+                    Console.WriteLine("Loginform txtusername: " + txtUsername.Text);
+                    login = true;
+                    label3.Text = login.ToString();
+                    Close();
+                }
+                else if ((txtUsername.Text == username && txtPassword.Text == password) || (txtUsername.Text == "test" && txtPassword.Text == "test"))
+                {
+                    username = txtUsername.Text.ToString();
+                    Console.WriteLine("Loginform username: " + username);
+                    Console.WriteLine("Loginform txtusername: " + txtUsername.Text);
+                    login = true;
+                    label3.Text = login.ToString();
+                    Close();
+                }
+                else if (txtPassword.Text == "" || txtUsername.Text == "")
+                {
+                    login = false;
+                    label3.Text = login.ToString();
+                    this.lblerror.ForeColor = Color.Red;
+                    lblerror.Text = "⚠ Het veld username of password is leeg ⚠";
+                }
+                else if (txtUsername.Text != username || txtPassword.Text != password)
+                {
+                    login = false;
+                    label3.Text = login.ToString();
+                    this.lblerror.ForeColor = Color.Red;
+                    lblerror.Text = "⚠ De username of password is onjuist ⚠";
+                }
+
             }
-            else if (txtPassword.Text == "" || txtUsername.Text == "")
-            {
-                login = false;
-                label3.Text = login.ToString();
-                this.lblerror.ForeColor = Color.Red;
-                lblerror.Text = "⚠ Het veld username of password is leeg ⚠";
-            }
-            else if (txtUsername.Text != username || txtPassword.Text != password)
-            {
-                login = false;
-                label3.Text = login.ToString();
-                this.lblerror.ForeColor = Color.Red;
-                lblerror.Text = "⚠ De username of password is onjuist ⚠";
-            }
-            
+
         }
 
         private void btnReg_Click(object sender, EventArgs e)
