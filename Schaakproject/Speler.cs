6595d4b66c641b211e415ac19f4629c0b01da1c5
RickSchoot <15090264@student.hhs.nl>
2016-03-29 15:49:38 +0200

    changes

--- Schaakproject/Speler.cs
+++ Schaakproject/Speler.cs
@@ -7,13 +7,17 @@ namespace Schaakproject
 {
     public abstract class Speler
     {
-        public string Naam { get; set; }
-        public string Kleur { get; set; }
+        public string Naam{ get; set; }
+
+        public string Kleur{ get; set; }
+
         public Spel spel { get; set; }
-        public Pion enPassantPion { get; set; }
+                
+        public Pion enPassantPion{ get; set; }
+
         public Koning Koning { get; set; }
         public int[] aantalstukken { get; set; }
-        public Pion[] pionnen { get; set;}
+        public Pion[] pionnen { get; set; }
 
         public Speler()
         {
@@ -21,7 +25,6 @@ namespace Schaakproject
             aantalstukken = new int[6] { 8, 2, 2, 2, 1, 16 };
             //0Pion      1Toren     2Paard     3Loper     4Dame      5Totaal
         }
-
     }
 }
 
