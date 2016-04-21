5c5f2bb803ad9b9c9d724906edbf534849ec89fb
Daan Nieuwenburg <15070735@student.hhs.nl>
2016-04-21 11:39:31 +0200

    Commentaar toegevoegd aan schaakstuk. (nu is het voor rob ook te begrijpen)

--- Schaakproject/Schaakstuk.cs
+++ Schaakproject/Schaakstuk.cs
@@ -15,26 +15,32 @@ namespace Schaakproject
         {
             if (Speler != null)
             {
+                // Zodra er een stuk geslagen word gaat er 1 van het totaal aantal stukken af
                 Speler.AantalStukken[5]--;
                 if (this is Pion )
                 {
+                    // Zodra er een Pion geslagen word gaat er 1 van het totaal aantal pionnen af
                     Speler.AantalStukken[0]--;
                     Geslagen = true;
                 }
                 else if (this is Toren )
                 {
+                    // Zodra er een Toren geslagen word gaat er 1 van het totaal aantal torens af
                     Speler.AantalStukken[1]--;
                 }
                 else if (this is Paard )
                 {
+                    // Zodra er een Paard geslagen word gaat er 1 van het totaal aantal paarden af
                     Speler.AantalStukken[2]--;
                 }
                 else if (this is Loper )
                 {
+                    // Zodra er een Loper geslagen word gaat er 1 van het totaal aantal lopers af
                     Speler.AantalStukken[3]--;
                 }
                 else if (this is Dame )
                 {
+                    // Zodra er een Dame geslagen word gaat er 1 van het totaal aantal dame(s) af
                     Speler.AantalStukken[4]--;
                 }
             }
